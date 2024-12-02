8ef5c24ce1f009f2e19213023716a2a3773cf235
NikhilKumar003 <112770631+NikhilKumar003@users.noreply.github.com>
2024-12-02 14:29:10 +0530

    Update CartPage

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -5,6 +5,6 @@ public class CartPage {
 	int q =30;
 	
 	public void getCart() {
-		System.out.println("get the details-- Naveen");
+		System.out.println("get the details-- Nikhil edit to override the confilts");
 	}
 }
